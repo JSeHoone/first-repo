163fba0786a43927f0c97ecfda94da0a856f1cb7
JSeHoone <beebea545@gmail.com>
2022-05-06 13:26:39 +0900

    print 'year' when i is time in 3 and print 'dream' time in 5

--- hello.py
+++ hello.py
@@ -6,6 +6,16 @@ for i in range(1,11):
         print("year")
     else:
         print(i)
+# 과제 2: 1부터 20까지 
+for x in range(1,21):
+    if x % 3 == 0:
+        print("year")
+    elif x % 5 == 0:
+        print("dream")
+    if x == 15:
+        print("yeardream")
+    else:
+        print(x)
 
 
 
